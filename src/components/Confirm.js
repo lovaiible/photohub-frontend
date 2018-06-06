dee8b31dd3d1c6c602374759c06bab05d32afd13
Martin <ga84xav@mytum.de>
2018-06-06 18:35:56 +0200

    Eingabe von Datum, zusätzlicher Beschreibung und Zahlungsmethode möglich

--- src/components/Confirm.js
+++ src/components/Confirm.js
@@ -16,6 +16,7 @@ import {
     TextField
 } from 'react-md';
 import ReactStars from 'react-stars'
+import { RadioGroup, RadioButton } from 'react-radio-buttons';
 
 
 import Page from './Page';
@@ -35,7 +36,44 @@ export class Confirm extends React.Component {
 
     constructor(props) {
         super(props);
+
+        this.state = {
+            payment: '',
+            date:'',
+            addInfo:''
+        };
+
+        this.handleChangePayment = this.handleChangePayment.bind(this);
+        this.handleChangeDate = this.handleChangeDate.bind(this);
+        this.handleChangeAddInfo = this.handleChangeAddInfo.bind(this);
+    //    this.handleSubmit = this.handleSubmit.bind(this);
+
     }
+
+
+
+    handleChangePayment(event){
+        this.setState({payment: event});
+    }
+
+    handleChangeDate(event){
+        this.setState({date: event});
+    }
+
+    handleChangeAddInfo(event){
+        this.setState({addInfo: event});
+    }
+
+    //handleSubmit(event){
+        //event.preventDefault();
+
+     //   booking.payment = this.state.payment;
+
+     //   this.props.onSubmit(booking)
+   // }
+
+
+
 
     render() {
         return <Page>
@@ -43,7 +81,9 @@ export class Confirm extends React.Component {
                 <CardTitle title="Confirm and Pay"
                            subtitle="Please confirm your booking details and select a payment method below."/>
 
-                <Grid className="grid-example">
+                <Grid className="grid-example"
+                      //onSubmit={this.handleSubmit()}
+                >
                     <Cell size={3}>
                         <Media aspectRatio="1-1">
                             <img src={this.props.movie.posters.detailed} alt={this.props.movie.title}/>
@@ -65,7 +105,8 @@ export class Confirm extends React.Component {
                 <CardText>
                     <h3>Please provide information below</h3>
                     <DatePicker
-                        id="appointment-date-auto"
+                        onChange={this.handleChangeDate}
+                        id="appointment-date"
                         required
                         label="Select an appointment date"
                         className="md-cell"
@@ -74,8 +115,10 @@ export class Confirm extends React.Component {
                         displayMode="landscape"
                         disableWeekEnds={true}
                         showAllDays={false}
-                    />
+                        cancelLabel={"Cancel"}
+                        okLabel={"Confirm"}/>
                     <TextField
+                        onChange={this.handleChangeAddInfo}
                         id="Additional Information"
                         label="Additional Information"
                         rows={2}
@@ -90,34 +133,38 @@ export class Confirm extends React.Component {
                 </CardText>
 
                 <Grid>
-                    <Cell size={3}>
+                    <Cell size={3} style={{display: 'flex', justifyContent: 'center'}}>
                         <a href="https://www.visa.com"
                            title="Select Visa as your payment method">
                             <img src={Visa}
                                  alt="Visa"
                                  width={150}
-                                 height={100}/>
+                                 height={100}
+                                 align="middle"
+                                 />
                         </a>
                     </Cell>
-                    <Cell size={3}>
+                    <Cell size={3} style={{display: 'flex', justifyContent: 'center'}}>
                         <a href="https://www.mastercard.com"
                            title="Select MasterCard as your payment method">
                             <img src={MasterCard}
                                  alt="MasterCard"
                                  width={150}
-                                 height={100}/>
+                                 height={100}
+                            />
                         </a>
                     </Cell>
-                    <Cell size={3}>
+                    <Cell size={3} style={{display: 'flex', justifyContent: 'center'}}>
                         <a href="https://www.paypal.com"
                            title="Select PayPal as your payment method">
                             <img src={PayPal}
                                  alt="PayPal"
                                  width={150}
-                                 height={100}/>
+                                 height={100}
+                            />
                         </a>
                     </Cell>
-                    <Cell size={3}>
+                    <Cell size={3} style={{display: 'flex', justifyContent: 'center'}}>
                         <a href={"https://www.klarna.com/sofort/"}
                            title="Select 'Sofortueberweisung' as your payment method">
                             <img src={Sofort}
@@ -128,6 +175,20 @@ export class Confirm extends React.Component {
                         </a>
                     </Cell>
                 </Grid>
+                <RadioGroup horizontal onChange={this.handleChangePayment}>
+                    <RadioButton value="Visa" rootColor={'#000000'} pointColor={'#000000'} iconSize={5}>
+                        <b>Visa</b>
+                    </RadioButton>
+                    <RadioButton value="Mastercard" rootColor={'#000000'} pointColor={'#000000'} iconSize={5}>
+                        <b>Mastercard</b>
+                    </RadioButton>
+                    <RadioButton value="Paypal" rootColor={'#000000'} pointColor={'#000000'} iconSize={5}>
+                        <b>Paypal</b>
+                    </RadioButton>
+                    <RadioButton value="Sofort" rootColor={'#000000'} pointColor={'#000000'}iconSize={5}>
+                        <b>Sofortüberweisung</b>
+                    </RadioButton>
+                </RadioGroup>
 
                 <CardText>
                     <h3>Cancellation Policy</h3>
@@ -150,6 +211,10 @@ export class Confirm extends React.Component {
                         </div>
                     </Cell>
                 </Grid>
+
+
+
+
 
 
             </Card>
