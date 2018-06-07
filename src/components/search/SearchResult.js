a728632aceb8d5ae240b837ffef32fdc8fea4f83
Anh Nguyen <anh.nguyen@tum.de>
2018-06-08 00:49:57 +0200

    Change file structure

--- src/components/search/SearchResult.js
+++ src/components/search/SearchResult.js
@@ -1,9 +1,9 @@
 "use strict"
 
 import React from 'react';
-import Page from './Page';
+import Page from '../Page';
 import SearchFields from "./SearchFields";
-import PaginationNav from "./Pagination";
+import PaginationNav from "../Pagination";
 import {SearchResultItem} from "./SearchResultItem";
 
 const data = [
